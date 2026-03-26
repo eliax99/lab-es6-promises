854d82cb28979592037a94ad6a10c6af0e64a843
Elias <eliasmedina99@hotmail.com>
2026-03-26 15:57:24 +0000

    completado sin extras

--- javascript/index.js
+++ javascript/index.js
@@ -4,6 +4,7 @@
 // 🚨🚨🚨 Comment out the below code before you start working on the code
 
 // Out of sync
+/*
   getInstruction("mashedPotatoes", 0, (step1) => {
     document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`;
   }, (error) => console.log(error));
@@ -24,17 +25,54 @@
     document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`;
     document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
   }, (error) => console.log(error));
+*/
+
+
+async function makeMashedPotatoes(){
+  // ${ } sirve para coger el valor dentro de la variable
+  for(let i=0; i<mashedPotatoes.length; i++){
+    let step = await obtainInstruction("mashedPotatoes",i)
+    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step}</li>`
+  }
+}
+
+makeMashedPotatoes();
 
 
 
-// Iteration 1 - using callbacks
-// ...
 
-// Iteration 2 - using promises
-// ...
+async function makeSteak(){
 
-// Iteration 3 using async/await
-// ...
+  for(let i=0; i<steak.length; i++){
+    let step = await obtainInstruction("steak",i)
+    document.querySelector("#steak").innerHTML += `<li>${step}</li>`
+  }
+}
 
-// Bonus 2 - Promise all
-// ...
+makeSteak();
+
+
+
+
+async function makeBroccoli(){
+
+  for(let i=0; i<broccoli.length; i++){
+    let step = await obtainInstruction("broccoli",i)
+    document.querySelector("#broccoli").innerHTML += `<li>${step}</li>`
+  }
+}
+
+makeBroccoli();
+
+
+
+
+async function makeBrusselsSprouts(){
+
+  for(let i=0; i<brusselsSprouts.length; i++){
+    let step = await obtainInstruction("brusselsSprouts",i)
+    document.querySelector("#brusselsSprouts").innerHTML += `<li>${step}</li>`
+  }
+}
+
+makeBrusselsSprouts();
